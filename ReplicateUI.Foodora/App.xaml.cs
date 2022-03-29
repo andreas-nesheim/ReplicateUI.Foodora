2095eaabe0d89613f51ebd6409c25133e6225477
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-03-29 13:19:21 +0200

    Add project files.

---
+++ ReplicateUI.Foodora/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReplicateUI.Foodora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ ReplicateUI.Foodora/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ReplicateUI.Foodora;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
