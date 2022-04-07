b24b2a705ec3e40d8a4a9bf01a89b783556c7e67
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-04-07 20:50:05 +0200

    change from CollectionView to CarouselView

--- ReplicateUI.Foodora/Page2.xaml.cs
+++ ReplicateUI.Foodora/Page2.xaml.cs
@@ -2,9 +2,12 @@
 
 public partial class Page2 : ContentPage
 {
+	public List<string> Items { get; set; } = new() { "hei", "test" };
+
 	public Page2()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 }
 
