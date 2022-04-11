c3f3b67a9185fae865f6c1465a29ebbbfcb96d27
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-04-11 12:47:55 +0200

    added new page

---
+++ ReplicateUI.Foodora/Page3.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ReplicateUI.Foodora;
+
+public partial class Page3 : ContentPage
+{
+	public List<string> Items { get; set; } = new() { "hei", "test" };
+
+	public Page3()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+}
+
