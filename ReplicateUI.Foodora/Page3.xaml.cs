dbde9f5dbd7e5448d5938d0f0e23b0c4b485fe09
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-04-18 22:47:41 +0200

    cleanup

--- ReplicateUI.Foodora/Page3.xaml.cs
+++ ReplicateUI.Foodora/Page3.xaml.cs
@@ -2,12 +2,9 @@
 
 public partial class Page3 : ContentPage
 {
-	public List<string> Items { get; set; } = new() { "hei", "test" };
-
 	public Page3()
 	{
 		InitializeComponent();
-		BindingContext = this;
 	}
 }
 
--- ReplicateUI.Foodora/Page4.xaml.cs
+++ ReplicateUI.Foodora/Page4.xaml.cs
@@ -2,12 +2,9 @@
 
 public partial class Page4 : ContentPage
 {
-	public List<string> Items { get; set; } = new() { "hei", "test" };
-
 	public Page4()
 	{
 		InitializeComponent();
-		BindingContext = this;
 	}
 }
 
