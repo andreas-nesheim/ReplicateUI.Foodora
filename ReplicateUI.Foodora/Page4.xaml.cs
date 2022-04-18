e539d42336b993efe0057ca343a39fbe9913df5a
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-04-18 22:45:24 +0200

    added page 4

---
+++ ReplicateUI.Foodora/Page4.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ReplicateUI.Foodora;
+
+public partial class Page4 : ContentPage
+{
+	public List<string> Items { get; set; } = new() { "hei", "test" };
+
+	public Page4()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+}
+
